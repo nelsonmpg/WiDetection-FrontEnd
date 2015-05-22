752899db31bb65c8e91f60c5e7ea81e5272e38a5
Nelson Gomes <nelsonmpg@hotmail.com>
2015-05-22 22:53:44 +0100

    Scipt para enviar o conteudo do ficheiroxml e programa em java para receber o conteudo do xml.

--- Wifi-Detect/src/Tests/WiFi/UDPReceive.java
+++ Wifi-Detect/src/Tests/WiFi/UDPReceive.java
@@ -7,6 +7,14 @@ package Tests.WiFi;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -15,17 +23,17 @@ import java.net.DatagramSocket;
 public class UDPReceive {
 
     public static void main(String args[]) {
+        HashMap<Integer, String> xmlDoc = new HashMap<>();
         try {
             int port = 80;
 
             // Create a socket to listen on the port.
             DatagramSocket dsocket = new DatagramSocket(port);
 
-      // Create a buffer to read datagrams into. If a
+            // Create a buffer to read datagrams into. If a
             // packet is larger than this buffer, the
             // excess will simply be discarded!
             byte[] buffer = new byte[4096];
-
             // Create a packet to receive data into the buffer
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
@@ -36,7 +44,18 @@ public class UDPReceive {
 
                 // Convert the contents to a string, and display them
                 String msg = new String(buffer, 0, packet.getLength());
-                System.out.println(msg);
+//                System.out.print(msg);
+                String[] a = msg.split("->");
+                xmlDoc.put(Integer.parseInt(a[0].trim()), a[1]);
+
+                if ("fim".equals(a[1].trim())) {
+                    xmlDoc = (HashMap<Integer, String>) sortByValue(xmlDoc);
+                    xmlDoc.remove(xmlDoc.size() - 1);
+                    for (Map.Entry<Integer, String> val : xmlDoc.entrySet()) {
+                        System.out.print(val.getValue());
+                    }
+                    xmlDoc.clear();
+                }
 
                 // Reset the length of the packet before reusing it.
                 packet.setLength(buffer.length);
@@ -44,5 +63,25 @@ public class UDPReceive {
         } catch (Exception e) {
             System.err.println(e);
         }
+
     }
+
+    public static Map sortByValue(Map unsortMap) {
+        List list = new LinkedList(unsortMap.entrySet());
+
+        Collections.sort(list, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                return ((Comparable) ((Map.Entry) (o1)).getKey())
+                        .compareTo(((Map.Entry) (o2)).getKey());
+            }
+        });
+
+        Map sortedMap = new LinkedHashMap();
+        for (Iterator it = list.iterator(); it.hasNext();) {
+            Map.Entry entry = (Map.Entry) it.next();
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+        return sortedMap;
+    }
+
 }
