929fb3cbb083fd5917fa005284025438b6ee7648
Rui Dias <ruifgdias@sapo.pt>
2015-07-16 23:30:02 +0100

    to merge

--- ProjetoFinal/www/js/HostArray.js
+++ ProjetoFinal/www/js/HostArray.js
@@ -20,7 +20,7 @@ HostArray.prototype.setImage = function (img) {
 };
 
 HostArray.prototype.listaHost = function () {
-    $("body").find(this.local).html("");
+    $("body").find("#" + this.local).html("");
     var self = this;
     for (var i = 0; i < this.arrayHosts.length; i++) {
         this.arrayElements.push(
@@ -32,7 +32,7 @@ HostArray.prototype.listaHost = function () {
                         self.image).createAndAddToDivHost());
     }
     if (this.arrayHosts.length == 0) {
-        $("body").find(this.local).append("<div class='jumbotron'><h2>N&atilde;o existem antenas ativas de momento</h2></div>");
+        $("body").find("#" + this.local).append("<div class='jumbotron'><h2>N&atilde;o existem antenas ativas de momento</h2></div>");
     }
 };
 
