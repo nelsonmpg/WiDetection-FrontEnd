847a0dc0f3305c3c733328622b9eeefd6b506f08
Rui Dias <ruifgdias@sapo.pt>
2015-07-20 17:45:16 +0100

    to sync

--- ProjetoFinal/www/js/scripts.js
+++ ProjetoFinal/www/js/scripts.js
@@ -511,14 +511,14 @@ function carregarDispAtivos(div, nomeAntena) {
 }
 
 
-function verificaDisp(array,mac){
+function verificaDisp(array, mac) {
     if (typeof array[mac] != "undefined") {
         return true;
     }
     return false;
 }
 
-function getSensores(array,mac){
+function getSensores(array, mac) {
     if (typeof array[mac] != "undefined") {
         return array[mac].sensores;
     }
@@ -529,12 +529,105 @@ function getSensores(array,mac){
  * @param {type} obj
  * @returns {Object}
  */
-function getTimeValue(obj){
+function getTimeValue(obj) {
     var temp = []
     if (typeof obj[0] != "undefined") {
         for (var q in obj) {
-            temp[obj[q].name]=obj[q].values;
+            temp[obj[q].name] = obj[q].values;
         }
         return temp;
-    }else return null;
+    } else
+        return null;
 }
+
+/**
+ * Passar uma data new Date("15/07/2015") e devolvolve um array com os MacAddress Ativos desde essa data
+ * @param {type} date
+ * @returns {Array|getMACAfterDate.entrou}
+ */
+function getMACAfterDate(date) {
+    var entrou = [];
+    for (var i in teste) {
+        for (var e in teste[i].sensores) {
+            for (var r in teste[i].sensores[e].values) {
+                if (new Date(teste[i].sensores[e].values[r].Last_time > date)) {
+                    entrou.push(teste[i].macAddress);
+                    break;
+                }
+            }
+        }
+    }
+    return entrou;
+}
+
+
+
+/**
+ * Passar uma data new Date("15/07/2015") e devolvolve um array com os MacAddress Ativos nessa data
+ * @param {type} date
+ * @returns {Array|getMACAfterDate.entrou}
+ */
+function getMACInDate(date) {
+    var entrou = [];
+    for (var i in teste) {
+        for (var e in teste[i].sensores) {
+            for (var r in teste[i].sensores[e].values) {
+                var find = new Date(teste[i].sensores[e].values[r].Last_time);
+                if (find.getDate() == date.getDate() && find.getFullYear() == date.getFullYear() && find.getHours() == date.getHours() && find.getMonth() == date.getMonth() && find.getMinutes() == date.getMinutes()) {
+                    entrou.push(teste[i].macAddress);
+                    break;
+                }
+            }
+        }
+    }
+    return entrou;
+}
+
+
+
+function makeCountFrom(date) {
+    var result = [];
+    while (date < new Date().addMinutes(-1)) {
+        result.push({x: new Date(date), y: (getMACInDate(date)).length});
+        date.addMinutes(1);
+    }
+
+    criarLightBox("ativos");
+
+
+    var chart = new CanvasJS.Chart("ativos",
+            {
+                theme: "theme2",
+                title: {
+                    text: "Dispositivos Ativos"
+                },
+                animationEnabled: true,
+                axisX: {
+                    valueFormatString: "H:mm:ss",
+                },
+                axisY: {includeZero: false},
+                data: [
+                    {
+                        type: "line",
+                        //lineThickness: 3,        
+                        dataPoints: result
+                    }
+
+
+                ]
+            });
+
+    chart.render();
+
+    return result;
+}
+
+Date.prototype.addHours = function (h) {
+    this.setHours(this.getHours() + h);
+    return this;
+};
+
+Date.prototype.addMinutes = function (h) {
+    this.setMinutes(this.getMinutes() + h);
+    return this;
+};
