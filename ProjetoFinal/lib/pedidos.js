74dd6098dc6625edd05ebfaa102a4d62a0f165bf
Rui Dias <ruifgdias@sapo.pt>
2015-07-26 16:19:28 +0100

    collapse chart calculation to pedidos.js

--- ProjetoFinal/lib/pedidos.js
+++ ProjetoFinal/lib/pedidos.js
@@ -6,6 +6,8 @@ var debug = require('debug')('r');
 var Worker = require('workerjs');
 
 var self = this;
+var liveActives = [];
+var intervalChart; // interval de update do gráfico dispositivos ativos
 
 exports.getNameVendorByMac = function (req, res) {
   console.log(self.getDataBase(req.params.sock));
@@ -126,4 +128,95 @@ exports.getNumDispositivos = function (req, res) {
       res.send(result);
     }
   });
+};
+
+/**
+ * 
+ * @param {type} req
+ * @param {type} res
+ * @returns {undefined}
+ */
+exports.getAllDisp = function (req,res){
+  if (liveActives[self.getDataBase(req.params.sock)] != undefined) {
+      res.json(liveActives[self.getDataBase(req.params.sock)]);
+    } else {
+      r.connect(self.dbData).then(function (conn) {
+        return r.db(self.getDataBase(req.params.sock)).table('DispMoveis').coerceTo("ARRAY").run(conn)
+                .finally(function () {
+                  conn.close();
+                });
+      }).then(function (output) {
+        var work = new Worker('./lib/workerGraph.js');
+        work.postMessage(output);
+        work.onmessage = function (msg) {
+          liveActives[self.getDataBase(req.params.sock)] = msg.data;
+          work.terminate();
+          res.json(msg.data);
+        };
+
+        //Interval de update do grafico nos clientes
+        intervalChart = setInterval(function () {
+          //a ultima data do array
+          var nextDate = new Date(liveActives[self.getDataBase(req.params.sock)][liveActives[self.getDataBase(req.params.sock)].length - 1].x);
+          // + 1 minuto, ou seja, r.now()
+          nextDate.addMinutes(1);
+          var min, hou;
+          // 
+          if (nextDate.getMinutes() != 0) {
+            min = 1;
+            hou = 0;
+          } else {
+            min = 1;
+            hou = 1;
+          }
+          
+            r.connect(self.dbData).then(function (conn) {
+              return r.db(self.getDataBase(req.params.sock)).table("DispMoveis").map(function (row) {
+                return  row("disp").do(function (ro) {
+                  return {"macAddress": row("macAddress"), "nameVendor": row("nameVendor"), "values": ro("values").nth(0).orderBy(r.desc("Last_time")).limit(10).orderBy(r.asc("Last_time"))}
+                })
+              }).map(function (a) {
+                return {"macAddress": a('macAddress'), "state": a('values').contains(function (value) {
+                    return r.now().inTimezone("+01:00").do(function (time) {
+                      return value('Last_time').ge(r.time(
+                              time.year(),
+                              time.month(),
+                              time.day(),
+                              time.hours().sub(hou),
+                              time.minutes().sub(min),
+                              time.seconds(),
+                              time.timezone()
+                              ));
+                    });
+                  })}
+              }).filter({"state": true}).count().run(conn)
+                      .finally(function () {
+                        conn.close();
+                      });
+            }).then(function (output) {
+
+              //Atualiza o array do servidor       
+              var novaData = new Date(liveActives[self.getDataBase(req.params.sock)][liveActives[self.getDataBase(req.params.sock)].length - 1].x);
+              novaData.addMinutes(1);
+              liveActives[self.getDataBase(req.params.sock)].shift();
+              var x = {x: novaData, y: output * 1};
+              liveActives[self.getDataBase(req.params.sock)].push(x);
+
+              //Envia para os clientes
+              self.io.sockets.emit("updateChart", self.getDataBase(req.params.sock), x);
+
+            }).error(function (err) {
+              res.status(500).json({err: err});
+            });
+        }, 1000 * 60); //De minuto a minuto
+      }).error(function (err) {
+        res.status(500).json({err: err});
+      });
+    }
+}
+
+
+Date.prototype.addMinutes = function (h) {
+  this.setMinutes(this.getMinutes() + h);
+  return this;
 };
