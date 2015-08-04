681f2f335e75c93036511eb38c1c2df724e74203
Rui Dias <ruifgdias@sapo.pt>
2015-08-04 19:42:05 +0100

    to sync

--- wifinder/www/js/utils.js
+++ wifinder/www/js/utils.js
@@ -1,48 +1,122 @@
-/* global CryptoJS, async, _ */
+  /* global CryptoJS, async, _ */
 
-var stringToMd5 = function (value) {
-  return CryptoJS.MD5(value).toString();
-};
+  var stringToMd5 = function (value) {
+      return CryptoJS.MD5(value).toString();
+  };
 
-var getKeyo = function () {
-  var ls = localStorage.getItem('keyo');
-  var ss = sessionStorage.getItem('keyo');
-  console.log(ls, ss);
-  return ls || ss;
-};
+  var getKeyo = function () {
+      var ls = localStorage.getItem('keyo');
+      var ss = sessionStorage.getItem('keyo');
+      console.log(ls, ss);
+      return ls || ss;
+  };
 
-window.templateLoader = {
-  load: function (views, callback) {
-    async.mapSeries(views, function (view, callbacki) {
-      if (window[view] === undefined) {
-        $.getScript('js/views/' + view.replace('View', '').toLowerCase() + '.js', function () {
-          if (window[view].prototype.template === undefined) {
-            $.get('templates/' + view + '.html', function (data) {
-              window[view].prototype.template = _.template(data);
-              callbacki();
-            }, 'html');
-          } else {
-            callbacki();
-          }
-        });
-      } else {
-        callbacki();
+  window.templateLoader = {
+      load: function (views, callback) {
+          async.mapSeries(views, function (view, callbacki) {
+              if (window[view] === undefined) {
+                  $.getScript('js/views/' + view.replace('View', '').toLowerCase() + '.js', function () {
+                      if (window[view].prototype.template === undefined) {
+                          $.get('templates/' + view + '.html', function (data) {
+                              window[view].prototype.template = _.template(data);
+                              callbacki();
+                          }, 'html');
+                      } else {
+                          callbacki();
+                      }
+                  });
+              } else {
+                  callbacki();
+              }
+          }, function (error, data) {
+              callback();
+          });
       }
-    }, function (error, data) {
-      callback();
-    });
-  }
-};
+  };
 
-window.modem = function (type, url, sucess, error, data) {
-  $.ajax({
-    async: true,
-    cache: false,
-    type: type || 'GET',
-    url: url,
-    dataType: 'json',
-    data: data,
-    success: sucess,
-    error: error
-  });
-};
+  var showmsg = function (local, tipo, msg, callback) {
+      var formsg = {
+          class: "",
+          icon: "",
+          titulo: ""
+      };
+      switch (tipo) {
+          case "success":
+              formsg = {
+                  class: "alert-success",
+                  icon: "fa-check",
+                  titulo: "Success!"
+              };
+              break;
+          case "error":
+              formsg = {
+                  class: "alert-error",
+                  icon: "fa-ban",
+                  titulo: "Error"
+              };
+              break;
+          case "warning":
+              formsg = {
+                  class: "alert-warning",
+                  icon: "fa-warning",
+                  titulo: "Warning"
+              };
+              break;
+      }
+      $("body").animate({
+          scrollTop: 0
+      });
+      $(local).html('<div class="col-md-8">' +
+          '<div class="box box-default">' +
+          '<div class="box-body">' +
+          '<div class="alert ' + formsg.class + ' alert-dismissable">' +
+          '<h3><i class="icon fa ' + formsg.icon + '"></i> ' + formsg.titulo + '</h3>' +
+          '<h4>' + msg + '</h4>' +
+          '</div></div></div></div>');
+      $(local).show();
+      setTimeout(function () {
+          $(local).hide();
+          $(local).html("");
+          if (callback) {
+              callback();
+          }
+      }, 3000);
+  };
+
+  var showInfoMsg = function (show, local, msg) {
+      var formsg = {
+          class: "alert-info",
+          icon: "fa-info",
+          titulo: "Info"
+      };
+
+      $("body").animate({
+          scrollTop: 0
+      });
+      if (show) {
+          $(local).html('<div class="col-md-8">' +
+              '<div class="box box-default">' +
+              '<div class="box-body">' +
+              '<div class="alert ' + formsg.class + ' alert-dismissable">' +
+              '<h3><i class="icon fa ' + formsg.icon + '"></i> ' + formsg.titulo + '</h3>' +
+              '<h4>' + msg + '</h4>' +
+              '</div></div></div></div>');
+          $(local).show();
+      } else {
+          $(local).hide();
+          $(local).html("");
+      }
+  };
+
+  window.modem = function (type, url, sucess, error, data) {
+      $.ajax({
+          async: true,
+          cache: false,
+          type: type || 'GET',
+          url: url,
+          dataType: 'json',
+          data: data,
+          success: sucess,
+          error: error
+      });
+  };
--- wifinder/www/js/views/editprofile.js
+++ wifinder/www/js/views/editprofile.js
@@ -1,15 +1,15 @@
-window.EditProfileView = Backbone.View.extend({
-    events: {
-        "click #btn-checkLogin": "check",
+  window.EditProfileView = Backbone.View.extend({
+      events: {
+          "click #btn-checkLogin": "check",
 //        "click #checkLogin button": "check",
-        'keyup .error': 'validateField'
-    },
-    initialize: function () {
-        //this.render();
-    },
-    init: function () {
-        $("#checkLogin").show();
-        $("#checkLogin > div > div > div > div > img , #img-previewProfile").attr("src", $(".imageuser").attr("src"));
+          'keyup .error': 'validateField'
+      },
+      initialize: function () {
+          //this.render();
+      },
+      init: function () {
+          $("#checkLogin").show();
+          $("#checkLogin > div > div > div > div > img , #img-previewProfile").attr("src", $(".imageuser").attr("src"));
 
 
 //    modem('GET',
@@ -22,42 +22,48 @@ window.EditProfileView = Backbone.View.extend({
 //                    error_launch(json.message);
 //                }, {}
 //        );
-    },
-    validateField: function (e) {
-        $(e.currentTarget).valid();
-    },
-    check: function (e) {
+      },
+      validateField: function (e) {
+          $(e.currentTarget).valid();
+      },
+      check: function (e) {
 //        e.stopImmediatePropagation();
-        e.preventDefault();
+          e.preventDefault();
 
-        modem('POST', "/login",
-            function (data) {
-                if (data.length > 0) {
-                    console.log(data);
-                    $("#editProfileDiv").show();
-                    $("#checkLogin").hide();
-                }
-            },
-            function (xhr, ajaxOptions, thrownError) {
-                var json = JSON.parse(xhr.responseText);
-                console.log(json.message);
+          modem('POST', "/login",
+              function (data) {
+                  if (data.length > 0) {
+                      console.log(data);
+                      $("#editProfileDiv").show();
+                      $("#checkLogin").hide();
+                  } else {
+                      showmsg(".my-modal", "error", "Login Failed!", function () {
+                          app.navigate("", {
+                              trigger: true
+                          });
+                      });
+                  }
+              },
+              function (xhr, ajaxOptions, thrownError) {
+                  var json = JSON.parse(xhr.responseText);
+                  console.log(json.message);
 
-                app.navigate("", {
-                    trigger: true
-                });
-            }, {
-            "email": $("#checkLogin input[type='text']").val(),
-            "pass": stringToMd5(btoa($("#checkLogin input[type='password']").val()))
-        });
+                  app.navigate("", {
+                      trigger: true
+                  });
+              }, {
+              "email": $("#checkLogin input[type='text']").val(),
+              "pass": stringToMd5(btoa($("#checkLogin input[type='password']").val()))
+          });
 
-    },
-    render: function () {
-        var self = this;
-        this.setValidator();
-        $(this.el).html(this.template());
-        return this;
-    },
-     setValidator: function(){
-           $("#checkLogin-form",this.el).validate();
-        }
-});
+      },
+      render: function () {
+          var self = this;
+          this.setValidator();
+          $(this.el).html(this.template());
+          return this;
+      },
+      setValidator: function () {
+          $("#checkLogin-form", this.el).validate();
+      }
+  });
